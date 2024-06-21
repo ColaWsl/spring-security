4bc3639ff3cd1a891d6019780d85e60a10d62a19
Austin <wsl612425@163.com>
2024-06-21 19:04:18 +0800

    获取用户认证信息

--- src/main/java/com/wangsl/controller/IndexController.java
+++ src/main/java/com/wangsl/controller/IndexController.java
@@ -1,8 +1,16 @@
 package com.wangsl.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -14,7 +22,20 @@ public class IndexController {
 
 	@ResponseBody
 	@GetMapping("/test")
-	public String test() {
-		return "hello";
+	public Map test() {
+		// 获取认证的相关信息
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+
+		Object principal = authentication.getPrincipal();
+		Object credentials = authentication.getCredentials();
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", authentication.getName());
+		map.put("principal", principal);
+		map.put("authorities", authorities);
+		return map;
 	}
+
 }
